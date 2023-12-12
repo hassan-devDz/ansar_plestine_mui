39e4d722ff560747e46955fda2c616c629e0509d
hassan-devDz <hassan26102017@gmail.com>
2023-12-12 01:42:41 +0100

    تغيير الشيما واضافة التحقق من البيانات

--- src/app/report-crime/api/route.js
+++ src/app/report-crime/api/route.js
@@ -1,81 +1,107 @@
 import { NextResponse, NextRequest } from "next/server";
 import path from "path";
 import formidable from "formidable";
-import { createValidationSchema as validationSchema ,schema} from "@/validation/ValidationSchema";
+import { schema } from "@/validation/ValidationSchema";
 import { createEdgeRouter, expressWrapper } from "next-connect";
 
+/**
+ * تحويل كائن FormData إلى كائن JavaScript مع التعامل مع حالة خاصة للمفتاح "responsibleParties"
+ * @param {FormData} form - كائن FormData الذي يجب تحويله إلى كائن JavaScript
+ * @returns {Object} - الكائن JavaScript بعد التحويل
+ */
 function formDataToObject(form) {
+  // الكائن النهائي الذي سيتم بناءه
   const obj = {};
+
+  // الدورة عبر مدخلات FormData
   for (const [key, value] of form.entries()) {
+    // تحويل المفاتيح إلى مصفوفة لمعالجة المؤشرات
     const keys = key.replace(/\]/g, "").split("[");
 
+    // استخدام reduce لبناء الكائن بناءً على المفاتيح
     keys.reduce((acc, currentKey, index) => {
       if (index === keys.length - 1) {
+        // إذا كان المفتاح هو "responsibleParties"
         if (currentKey === "responsibleParties") {
           // تقسيم القيمة إلى مصفوفة عند الفواصل
           acc[currentKey] = value.split(",");
         } else if (acc[currentKey]) {
+          // إذا كان هناك قيمة بالفعل، قم بإضافتها إلى مصفوفة إذا لزم الأمر
           if (!Array.isArray(acc[currentKey])) {
             acc[currentKey] = [acc[currentKey]];
           }
+
           acc[currentKey].push(value);
+        } else if (currentKey === "files") {
+          // إذا كان هناك قيمة بالفعل، قم بإضافتها إلى مصفوفة إذا لزم الأمر
+          if (!Array.isArray(value)) {
+            acc[currentKey] = [value];
+          }
         } else {
+          // إلا، قم بتعيين القيمة
           acc[currentKey] = value;
         }
       } else {
+        // إذا لم تكن هذه آخر مؤشر، قم بتكوين الكائن الداخلي
         acc[currentKey] = acc[currentKey] || {};
       }
       return acc[currentKey];
     }, obj);
   }
+
   return obj;
 }
 
-// Middleware للتحقق من البيانات
+/**
+ * Middleware للتحقق من البيانات باستخدام Yup
+ * @param {Object} req - كائن الطلب
+ * @param {Object} res - كائن الاستجابة
+ * @param {Function} next - وظيفة التابع القادمة في سلسلة المتوسطات
+ * @returns {Promise} - إذا تم التحقق بنجاح، تابع التنفيذ، وإلا فإنه يرسل رد خطأ
+ */
 async function validateFormData(req, res, next) {
   try {
     // معالجة multipart/form-data
-     const formData = await req.formData();
-     const fields = formDataToObject(formData)
-    console.log(fields, "ffffff");
+    const formData = await req.formData();
+
+    // تحويل FormData إلى كائن JavaScript
+    const fields = await formDataToObject(formData);
     // التحقق من البيانات باستخدام Yup
-    await schema.validate(fields, { abortEarly: false });
+    await schema
+      .camelCase()
+      .validate(fields, { abortEarly: false, stripUnknown: true });
 
     // إذا تم التحقق بنجاح، تابع التنفيذ
     return next();
   } catch (error) {
     // إرسال رسالة الخطأ إذا فشل التحقق
-    console.log(error)
-    return NextResponse.json(
-          { error: `${ error.message}` },
-          { status: 400 }
-        );
-    
+    console.error(error);
+    return NextResponse.json({ error: `${error.message}` }, { status: 400 });
   }
 }
 
-//
-
 const router = createEdgeRouter();
 
-router
- .use(validateFormData)
-  .post(async (req, res) => {
-    return NextResponse.json({
+router.use(validateFormData).post(async (req, res) => {
+  return NextResponse.json(
+    {
       message: "User has been created",
-    });
-  });
+    },
+    { status: 201 }
+  );
+});
 router.handler({
   onError(error, req, res) {
-    return NextResponse.json({ error: `Sorry something Happened! ${error.message}` }, { status: 501 });
-   
+    return NextResponse.json(
+      { error: `Sorry something Happened! ${error.message}` },
+      { status: 501 }
+    );
   },
   onNoMatch(req, res) {
-        return NextResponse.json(
-          { error: `Method '${req.method}' Not Allowed` },
-          { status: 405 }
-        );
-
+    return NextResponse.json(
+      { error: `Method '${req.method}' Not Allowed` },
+      { status: 405 }
+    );
   },
 });
 export const config = {
@@ -87,46 +113,3 @@ export const runtime = "nodejs";
 export async function POST(request, ctx) {
   return router.run(request, ctx);
 }
-// const yup = require('yup');
-// router.get((req) => {
-//   return NextResponse.json({ message:"ok" });
-// });
-// // تعريف قواعد التحقق باستخدام Yup
-// const validationSchema = yup.object().shape({
-//   username: yup.string().required(),
-//   email: yup.string().email().required(),
-//   // قواعد التحقق الأخرى...
-// });
-
-// // استخدام middleware للتحقق من البيانات
-// app.post('/upload', validateFormData, (req, res) => {
-//   // معالجة البيانات بعد التحقق
-//   const { fields } = req;
-//   // القيام بما تحتاج إليه مع البيانات المتحققة
-//   res.status(200).json({ success: true, data: fields });
-// });
-
-// استخدام Express أو أي إطار عمل آخر
-export function validate(schema, handler) {
-  return async (req, res) => {
-    if (["POST", "PUT"].includes(req.method)) {
-      try {
-        req.body = await schema
-          .camelCase()
-          .validate(req.body, { abortEarly: false, stripUnknown: true });
-      } catch (error) {
-        return res.status(400).json(error);
-      }
-    }
-    await handler(req, res);
-  };
-}
-//export async function POST(request) {
-//   const formData = await request.formData();
-//   for (let pair of formData.entries()) {
-//     console.log(pair[0] + ", " + pair[1]);
-//   }
-//   const file = formData.getAll("files");
-
-//   return Response.json({ message: formData.keys() });
-// }
